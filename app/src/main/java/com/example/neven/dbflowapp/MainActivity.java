f6e8d36a24845c303ad3735fc9a9865f23a3094f
Neven <nevenmagdalenic1993@gmail.com>
2017-01-18 14:23:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/neven/dbflowapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.neven.dbflowapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "welcome to db flow app!", Toast.LENGTH_SHORT).show();
+    }
+}
